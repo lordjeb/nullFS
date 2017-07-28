5c7f60d3eb2038861e099eda03d7aca462c0471e
Jeremy Hurren <lordjeb@lordjeb.com>
2017-07-27 22:31:58 -0600

    Updates to testing environment

--- build.cmd
+++ build.cmd
@@ -1,4 +1,9 @@
 @Echo off
+setlocal
+
+set VMTARGET=WIN10-TEST
 msbuild.exe nullFS.sln /m /t:Rebuild /p:Platform=x64 /p:Configuration=Debug /v:minimal
-net use \\vm-win8\nullfs /user:Share asdf
-robocopy bld \\vm-win8\nullfs\bld /MIR /NJH /NJS /W:3 /R:10
+net use \\%VMTARGET%\nullFS /user:%VMTARGET%\Admin F5L0gix
+robocopy bld \\%VMTARGET%\nullFS\bld /MIR /NJH /NJS /W:3 /R:10
+
+endlocal
