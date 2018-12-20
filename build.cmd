38ad8969a4716752d842dcc33c321e70c7b2dc12
Jeremy Hurren <lordjeb@lordjeb.com>
2018-12-19 21:03:05 -0700

    Remove obsolete files

--- build.cmd
+++
@@ -1,9 +0,0 @@
-@Echo off
-setlocal
-
-set VMTARGET=WIN10-TEST
-msbuild.exe nullFS.sln /m /t:Build /p:Platform=x64 /p:Configuration=Debug /v:minimal
-net use \\%VMTARGET%\nullFS /user:%VMTARGET%\Admin F5L0gix
-robocopy bld \\%VMTARGET%\nullFS\bld /MIR /NJH /NJS /W:3 /R:10
-
-endlocal
