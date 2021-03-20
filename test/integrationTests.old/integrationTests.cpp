1435e69cc9e0804888bac7689be45d4202ba317e
Jeremy Hurren <lordjeb@lordjeb.com>
2021-03-20 15:34:10 -0600

    Fix output directory

--- test/integrationTests.old/integrationTests.cpp
+++
@@ -1,8 +0,0 @@
-#include "NullFsDriverEnvironment.h"
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::AddGlobalTestEnvironment(new NullFsDriverEnvironment);
-    return RUN_ALL_TESTS();
-}
