bd3b91dd039be719fc7698afa51d41877d14b224
Jeremy Hurren <lordjeb@lordjeb.com>
2021-09-24 11:15:11 -0600

    No code analysis on google test projects

--- test/integrationTests/pch.h
+++ test/integrationTests/pch.h
@@ -4,7 +4,11 @@
 
 #include <wil/resource.h>
 
+#pragma warning(push)
+#include <CodeAnalysis\warnings.h>
+#pragma warning(disable : ALL_CODE_ANALYSIS_WARNINGS)
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#pragma warning(pop)
 
 #include <Windows.h>
